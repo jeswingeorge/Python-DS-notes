7ae43d0e24bbbc6981bc584344cb53742ef958f1
Jeswin George <30544734+jeswingeorge@users.noreply.github.com>
2023-05-01 22:00:11 +0530

    completed till pass by ref

---
+++ Python OOPs/reading.md
@@ -0,0 +1,5 @@
+## Reading material
+
+1. [Python documnetation classes](https://docs.python.org/3/tutorial/classes.html#a-first-look-at-classes)
+    Also, in the documentation refer to data class.
+
--- Python OOPs/temp.py
+++ Python OOPs/temp.py
@@ -1,20 +1,54 @@
-def purchase_mobile(price,mobile_brand):
-    if mobile_brand == "Apple":
-            discount = 10
-    else:
-        discount = 5
-    
-    total_price = price - price * discount / 100
-    print("Total price of "+ 'mobile' +" is "+str(total_price))
+class Student:
+    def __init__(self,student_id,age,marks) -> None:
+        self.__student_id = student_id
+        self.__age = age
+        self.__marks = marks
 
-def purchase_shoe(price, material):
-    if material == "leather":
-        tax = 5
-    else:
-        tax = 2
-    
-    total_price = price + price * tax / 100
-    print("Total price of "+ 'shoe' +" is "+str(total_price))
+    ## Setter methods
+    def set_student_id(self, student_id):
+        self.__student_id = student_id
 
-purchase_mobile(20000,"Apple")
-purchase_shoe(100,'leather')
+    def set_age(self, age):
+        self.__age = age
+
+    def set_marks(self, marks):
+        self.__marks = marks
+
+    ## Getter methods
+    def get_student_id(self):
+        return(self.__student_id)
+    
+    def get_age(self):
+        return(self.__age)
+    
+    def get_marks(self):
+        return(self.__marks)
+    
+    ## Other methods
+
+    def validate_marks(self):
+        if (0<= self.get_marks() <= 100):
+            return(True)
+        else:
+            return(False)
+        
+    def validate_age(self):
+        if self.get_age() > 20:
+            return(True)
+        else:
+            return(False)
+     
+    def check_qualification(self):
+        if self.validate_age() and self.validate_marks():
+            if self.get_marks()>=65:
+                print("Student is eligible")
+                return(True)
+            else:
+                return(False)
+        print("Student is not eligible")
+        return(False)
+
+        
+jeswin = Student('123', 31, 76)
+jeswin.check_qualification()
+    
