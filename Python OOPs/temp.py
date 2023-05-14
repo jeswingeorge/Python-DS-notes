aa0d779b8987a44a8720d3205bf8a428be96a204
Jeswin George <30544734+jeswingeorge@users.noreply.github.com>
2023-05-14 11:43:10 +0530

    added static notes

--- Python OOPs/temp.py
+++ Python OOPs/temp.py
@@ -1,54 +1,24 @@
-class Student:
-    def __init__(self,student_id,age,marks) -> None:
-        self.__student_id = student_id
-        self.__age = age
-        self.__marks = marks
-
-    ## Setter methods
-    def set_student_id(self, student_id):
-        self.__student_id = student_id
-
-    def set_age(self, age):
-        self.__age = age
-
-    def set_marks(self, marks):
-        self.__marks = marks
-
-    ## Getter methods
-    def get_student_id(self):
-        return(self.__student_id)
-    
-    def get_age(self):
-        return(self.__age)
-    
-    def get_marks(self):
-        return(self.__marks)
-    
-    ## Other methods
-
-    def validate_marks(self):
-        if (0<= self.get_marks() <= 100):
-            return(True)
-        else:
-            return(False)
+class Customer:
+    def __init__(self, cust_id, cust_name, location):
+        self.cust_id = cust_id
+        self.cust_name = cust_name
+        self.location = location
         
-    def validate_age(self):
-        if self.get_age() > 20:
-            return(True)
-        else:
-            return(False)
-     
-    def check_qualification(self):
-        if self.validate_age() and self.validate_marks():
-            if self.get_marks()>=65:
-                print("Student is eligible")
-                return(True)
-            else:
-                return(False)
-        print("Student is not eligible")
-        return(False)
+    def __str__(self):
+        return(f"customer id: {self.cust_id}, customer name: {self.cust_name}, location: {self.location}")
 
-        
-jeswin = Student('123', 31, 76)
-jeswin.check_qualification()
-    
+list_of_customers = [Customer(101, 'Mark', 'US'),
+Customer(102, 'Jane', 'Japan'),
+Customer(103, 'Kumar', 'India')]
+
+dict_of_customer={}
+
+for obj in list_of_customers:
+    dict_of_customer[obj.location] = obj
+
+# print(dict_of_customer.keys())
+
+# print(dict_of_customer['US'])
+
+for key in dict_of_customer.keys():
+    print(key,' : ',dict_of_customer[key])
