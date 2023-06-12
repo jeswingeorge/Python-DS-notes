07de7e597d9f54a24174563eb7b7928fa1f909d5
Jeswin George <30544734+jeswingeorge@users.noreply.github.com>
2023-06-12 19:47:17 +0530

    added animation plots data

--- Python OOPs/temp.py
+++ Python OOPs/temp.py
@@ -1,24 +1,56 @@
-class Customer:
-    def __init__(self, cust_id, cust_name, location):
-        self.cust_id = cust_id
-        self.cust_name = cust_name
-        self.location = location
-        
-    def __str__(self):
-        return(f"customer id: {self.cust_id}, customer name: {self.cust_name}, location: {self.location}")
+class Ticket:
+    counter = 0
+    def __init__(self,passenger_name,source,destination) -> None:
+        self.__passenger_name = passenger_name
+        self.__source = source
+        self.__destination = destination
+        self.__ticket_id = self.generate_ticket()
 
-list_of_customers = [Customer(101, 'Mark', 'US'),
-Customer(102, 'Jane', 'Japan'),
-Customer(103, 'Kumar', 'India')]
+    def __str__(self) -> str:
+        return(f"Ticket_id: {self.get_ticket_id()}, Passenger name: {self.get_passenger_name()}, Source of journey: {self.get_source()}, Destination: {self.get_destination()}")
+    
+    def get_ticket_id(self):
+        return self.__ticket_id
+    
+    def get_passenger_name(self):
+        return(self.__passenger_name)
+    
+    def get_source(self):
+        return self.__source
+    
+    def get_destination(self):
+        return self.__destination
+    
+    def validate_source_destination(self):
+        if self.get_source().lower()=="delhi":
+            if self.get_destination().lower()=='mumbai' or self.get_destination().lower()=='chennai' or self.get_destination().lower()=='pune' or self.get_destination().lower()=='kolkata':
+                return True
+            
+        else:
+            print("Source must be Delhi")
+            return False
+    
+    def generate_ticket(self):
+        if self.validate_source_destination():
+            Ticket.counter += 1
+            prefix = self.get_source().upper()[0] + self.get_destination().upper()[0]
+            if len(str(Ticket.counter))==1:
+                self.__ticket_id = prefix + '0' +  str(Ticket.counter)
+            else:
+                self.__ticket_id = prefix +  str(Ticket.counter)
 
-dict_of_customer={}
+            return self.__ticket_id
 
-for obj in list_of_customers:
-    dict_of_customer[obj.location] = obj
+        else:
+            self.__ticket_id = None
+            return self.__ticket_id
 
-# print(dict_of_customer.keys())
 
-# print(dict_of_customer['US'])
 
-for key in dict_of_customer.keys():
-    print(key,' : ',dict_of_customer[key])
+    
+
+t1 = Ticket("jvg",'Kerala','Pune')
+print(t1)
+
+
+    
